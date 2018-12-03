dee6ac9332df5ec20eed5da5ab186495ae584259
MC Le Morvan <mclemorvan@gmail.com>
2018-12-03 17:39:14 +0100

    working through linking db and scrapper

--- app/services/scrapping.rb
+++ app/services/scrapping.rb
@@ -8,10 +8,32 @@ class Scrapping
   end
 
 
+  def perform
+    scrapping_genre('http://everynoise.com')
+  end
+
+
+  # Method that scraps genres and their URL into an hash
+  def scrapping_genre(url)
+    doc = Nokogiri::HTML(open(url))
+    genres = {}
+
+    # To create our genre database + links
+    doc.css("div.genre").each do |div|
+      genre = (div.text).tr('»', '')
+      id = Genre.create(name: genre).id
+      link = 'http://everynoise.com/' + (div.css('a')[0]["href"])
+     #scraps artists info
+      scrapping_artists(link, id)
+    end
+  end
+
+
   # Method that creates a database of artists with their spotify_Id and genres
   # from a everynoise genre URL
-  def scrapping_artists(url, genre)
+  def scrapping_artists(url, id)
     doc = Nokogiri::HTML(open(url))
+    @id = id
 
     # Goes through all genre divs
     doc.css("div.genre").each do |div|
@@ -20,31 +42,11 @@ class Scrapping
         spotify = div.css('a')[0]["href"].tr('spotify:artist:', '')
         name = div.text.tr('»', '')
       # create an hash with artist name => [spotify_Id, genre]
-        @artists[name] = [spotify, genre]
+        a = Artist.create(name: name, spotify_id: spotify)
+        a.genres << Genre.find(@id)
       end
     end
   end
 
-  # Method that scraps genres and their URL into an hash
-  def scrapping_genre(url)
-    doc = Nokogiri::HTML(open(url))
-    genres = {}
-    @artists = {}
-
-    # To create our genre database + links
-    doc.css("div.genre").each do |div|
-      genre = (div.text).tr('»', '')   #genre name
-      link = 'http://everynoise.com/' + (div.css('a')[0]["href"])  #genre url
-      scrapping_artists(link, genre)  #scraps artists info
-      genres[genre] = link
-      puts genres #hash de type {genrename = url}
-      puts @artists #hash de type {artistname = spotify_id, genre}
-    end
-  end
-
-
-  def perform
-    scrapping_genre('http://everynoise.com')
-  end
 
 end

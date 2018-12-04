03f1eccfae8cf6500d7fade63b8f41ba4453c692
charles_mahaco <charles.belpois@gmail.com>
2018-12-04 13:50:49 +0100

    Added Spotify API features on artists' show & controller + Spotify test service

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,11 @@
 class ArtistsController < ApplicationController
   def show
+	  RSpotify::authenticate("2fc8c7db0a584ecc97c8789e10b1ba14", "3e31ba14f979474ab69880fafd410829")
+	  if params[:artist] != nil
+	    @artists = RSpotify::Artist.search(params[:artist]).first
+	    @image = @artists.images[0]['url']
+	    @similar = @artists.related_artists
+      end
   end
+ 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,8 @@
 class HomeController < ApplicationController
 
 	def index
-		RSpotify::authenticate("2fc8c7db0a584ecc97c8789e10b1ba14", "3e31ba14f979474ab69880fafd410829")
-		if params[:artist] != nil
-		@artists = RSpotify::Artist.search(params[:artist]).first
-		
-		@image = @artists.images[0]['url']
-		puts @image
+	
+
 	    end
 	end
 	

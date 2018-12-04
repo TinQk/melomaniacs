5ca54745a4aac05c096e5955cbee628072faa2c6
TinQk <potie.quentin@gmail.com>
2018-12-04 15:58:55 +0100

    fix controller artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,8 +2,6 @@ class ArtistsController < ApplicationController
   def show
 
     @artist = Artist.find(params[:id])
-    puts '@artist.spotify_id'
-    puts '#{@artist.spotify_id}'
 
     RSpotify::authenticate("2fc8c7db0a584ecc97c8789e10b1ba14", "3e31ba14f979474ab69880fafd410829")
     if @artist.spotify_id != nil
@@ -11,6 +9,8 @@ class ArtistsController < ApplicationController
       @image = @artist_spotify.images[0]['url']
       @similar = @artist_spotify.related_artists
     end
+
+    @likes = @artist.likes.count
 
   end
 

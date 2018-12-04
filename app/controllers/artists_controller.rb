652afa6a0ed1ce55639badcaa485fc782fe538e1
TinQk <potie.quentin@gmail.com>
2018-12-04 15:20:54 +0100

    artists show alpha release

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,17 @@
 class ArtistsController < ApplicationController
   def show
-	  RSpotify::authenticate("2fc8c7db0a584ecc97c8789e10b1ba14", "3e31ba14f979474ab69880fafd410829")
-	  if params[:artist] != nil
-	    @artists = RSpotify::Artist.search(params[:artist]).first
-	    @image = @artists.images[0]['url']
-	    @similar = @artists.related_artists
-      end
+
+    @artist = Artist.find(params[:id])
+    puts '@artist.spotify_id'
+    puts '#{@artist.spotify_id}'
+
+    RSpotify::authenticate("2fc8c7db0a584ecc97c8789e10b1ba14", "3e31ba14f979474ab69880fafd410829")
+    if @artist.spotify_id != nil
+      @artist_spotify = RSpotify::Artist.find(@artist.spotify_id)
+      @image = @artist_spotify.images[0]['url']
+      @similar = @artist_spotify.related_artists
+    end
+
   end
- 
+
 end

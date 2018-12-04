86a71aaf2e79acec95b4773ef4ee3b7922a71bd5
TinQk <potie.quentin@gmail.com>
2018-12-04 16:53:19 +0100

    structure show artist and genre

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,7 +5,7 @@ class ArtistsController < ApplicationController
 
     RSpotify::authenticate("2fc8c7db0a584ecc97c8789e10b1ba14", "3e31ba14f979474ab69880fafd410829")
     if @artist.spotify_id != nil
-      @artist_spotify = RSpotify::Artist.find(@artist.spotify_id)
+      @artist_spotify = RSpotify::Artist.find('6X66Odq1u17bBzTdYsL7Y7')
       @image = @artist_spotify.images[0]['url']
       @similar = @artist_spotify.related_artists
     end

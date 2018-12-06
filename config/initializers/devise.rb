edf3c3b46dad429728d8f083c81f712a26143984
AndreaPlm <palma.a.andrea@gmail.com>
2018-12-06 17:34:18 +0100

    add line 17-20: config for custom failure

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,11 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
 
+  # CUSTOM FAILURE
+    config.warden do |manager|
+      manager.failure_app = CustomFailure
+    end
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class

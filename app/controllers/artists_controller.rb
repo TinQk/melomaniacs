d553fc20c218e6fdc9e2cff386904ca062628220
AndreaPlm <palma.a.andrea@gmail.com>
2018-12-07 19:33:13 +0100

    fixed similar artists links, and few css changes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,6 +5,8 @@ class ArtistsController < ApplicationController
 
     @artist = Artist.find(params[:id])
 
+    @all = Artist.all
+
     RSpotify::authenticate("2fc8c7db0a584ecc97c8789e10b1ba14", "3e31ba14f979474ab69880fafd410829")
     if @artist.spotify_id != nil
       @artist_spotify = RSpotify::Artist.find("#{@artist.spotify_id}")
@@ -13,6 +15,8 @@ class ArtistsController < ApplicationController
       @image = @artist_spotify.images[0]['url']
       puts @image
       @similar = @artist_spotify.related_artists
+
+
     end
 
     @likes = @artist.likes.count

4adf64616268ffe1068347859e9f969e90f37997
TinQk <potie.quentin@gmail.com>
2018-12-11 12:26:55 +0100

    switch from dotenv to rails credentials

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'], api_key: ENV['ADMIN_API_KEY'] }
+AlgoliaSearch.configuration = { application_id: Rails.application.credentials.algoliasearch_application_id, api_key: Rails.application.credentials.algoliasearch_admin_api_key }

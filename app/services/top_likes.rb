12dc6208216cb0f898f9b4aab2bdf73134983eab
MC Le Morvan <mclemorvan@gmail.com>
2018-12-11 18:59:28 +0100

    fixed genre buttons decoration

---
+++ app/services/top_likes.rb
@@ -0,0 +1,22 @@
+$newarr=[]
+
+class TopLikes
+  def initialize
+  end
+
+  def get_top
+    array = []
+    array = Artist.all.sort_by{|a| a.likes.count}.reverse
+    $newarr << array[0]
+    $newarr << array[1]
+    $newarr << array[2]
+    $newarr << array[3]
+    $newarr << array[4]
+    $newarr << array[5]
+    $newarr << array[6]
+    $newarr << array[7]
+    $newarr << array[8]
+    $newarr << array[9]
+    return $newarr
+  end
+end

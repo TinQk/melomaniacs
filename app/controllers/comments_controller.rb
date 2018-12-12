2b424f73ac12e869adc418928e25be12f8ecceb6
TinQk <potie.quentin@gmail.com>
2018-12-12 23:16:06 +0100

    add routes, add controllers and view comments but not finished

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    result = comment_params
+    comment = Comment.create(result)
+    redirect_to(artist_path(params[:artist_id]))
+  end
+
+  def edit
+    unless @current_user
+      redirect_to(root_path)
+    end
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to(artist_path(params[:artist_id]))
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to(artist_path(params[:artist_id]))
+  end
+
+  private
+
+  def comment_params
+    result = params.require(:comment).permit(:content)
+    result[:user_id] = current_user.id
+    return result
+  end
+end

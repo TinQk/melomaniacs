82edf4f3d1419e7e3411ee265bd7a76cb02c466d
TinQk <potie.quentin@gmail.com>
2018-12-13 23:29:44 +0100

    comment home controller to fix bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,8 @@ class HomeController < ApplicationController
 	def discover
 	  @url = []
 	  RSpotify::authenticate(Rails.application.credentials.spotify_client_id, Rails.application.credentials.spotify_client_secret)
-	  Toplike.all.count.times do |i|
-	    @url << RSpotify::Artist.find("#{Artist.find(Toplike.all[i].id).spotify_id}")
-	  end
+	  #Toplike.all.count.times do |i|
+	  #  @url << RSpotify::Artist.find("#{Artist.find(Toplike.all[i].id).spotify_id}")
+	  #end
 	end
 end

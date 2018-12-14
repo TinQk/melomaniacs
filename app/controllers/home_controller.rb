318372f086e56bcb9ac7d8c5e4152df04be43c3a
charles_mahaco <charles.belpois@gmail.com>
2018-12-14 17:32:14 +0100

    slightly optimized discover page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,6 @@ class HomeController < ApplicationController
 	  @user_likes = current_user.likes.all.reverse
 	  RSpotify::authenticate(Rails.application.credentials.spotify_client_id, Rails.application.credentials.spotify_client_secret)
 
-	  Toplike.count.times do |i|
-	    @top_artists << Artist.find(Toplike.all[i].id)
-	  end
-
 	  @user_likes.each do |dupli|
 	  	@duplicates << dupli.artist.name
 	  end

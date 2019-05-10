2341d587de233d970b1d935633f02ea92215455f
TinQk <potie.quentin@gmail.com>
2019-05-10 19:29:46 +0200

    add search by genre home

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.my_form').keyup(function () {
+  $('.artist_form').keyup(function () {
     var search = $(this).val();
     if (search != '') {
       var datObject;
@@ -7,7 +7,7 @@ $(document).ready(function () {
         url: '/autocomplete',
         method: 'GET',
         dataType: 'json',
-        data: { term: search },
+        data: { what: 'artists', term: search },
         success: function (data) {
           $('#artists-hits').empty();
           var len = data['data'].length;
@@ -22,6 +22,34 @@ $(document).ready(function () {
       });
     } else {
       $('#artists-hits').empty();
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('.genre_form').keyup(function () {
+    var search = $(this).val();
+    if (search != '') {
+      var datObject;
+      $.ajax({
+        url: '/autocomplete',
+        method: 'GET',
+        dataType: 'json',
+        data: { what: 'genres', term: search },
+        success: function (data) {
+          $('#genres-hits').empty();
+          var len = data['data'].length;
+          for (var i = 0; i < len; i++) {
+            var id = data['data'][i]['id'];
+            var name = data['data'][i]['name'];
+            if (len > 0) {
+              $('#genres-hits').append('<a href="/genres/' + id + '"><li>' + name + '</li></a>');
+            }
+          }
+        },
+      });
+    } else {
+      $('#genres-hits').empty();
     }
   });
 });
